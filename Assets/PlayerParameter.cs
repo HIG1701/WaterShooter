c50983585bb6c667c71bdab618aa50d7c47a5744
HIG1701 <masarunwatarun@gmail.com>
2024-10-21 10:16:46 +0900

    Parameter追加

--- Assets/PlayerParameter.cs
+++ Assets/PlayerParameter.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObject/PlayerParameter")]
 public class PlayerParameter : ScriptableObject
 {
+    //プレイヤーの設定に関するParameter
+    public int PlayerID;                    //プレイヤーID
+    public string PlayerName;               //プレイヤー名
+    public string WeaponName;               //武器名
+    public float PlayerHeight;              //プレイヤー身長
+
+    //プレイヤーのInGameに影響するParameter
+    public float PlayerHP;                  //体力
+    public float PlayerShield;              //シールド耐久力
+    public float PlayerAttack;              //攻撃力
+    public float AttackSpeed;               //攻撃速度
+    public float AttackDistance;            //射程距離
+    public float ReloadSpeed;               //リロード速度
+    public int Coin;                        //コイン枚数
+
     //速度に関するParameter
     public float PlayerSpeed;               //通常速度
     public float SprintSpeed;               //加速
@@ -12,5 +27,4 @@ public class PlayerParameter : ScriptableObject
 
     //重力に関するParameter
     public float GravityMultiplier;         //このオブジェクトのみ、重力の影響を強める
-
 }
