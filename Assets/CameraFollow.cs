19ff3d31edfc12166164e6e8cb8b96bbfb312173
HIG1701 <masarunwatarun@gmail.com>
2024-10-23 23:17:52 +0900

    カメラが壁にめり込まないようにした

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -9,6 +9,9 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Vector3 Offset;                                //カメラのオフセット
     [SerializeField] float Sensitivity = 5f;                        //マウス感度
     [SerializeField] float Distance = 5f;                           //プレイヤーからの距離
+    [SerializeField] float SphereCastRadius = 0.5f;                 //SphereCastの半径
+    [SerializeField] LayerMask CollisionLayers;                     //衝突を検出するレイヤー（インスペクターでWallを指定）
+
 
     private float CurrentX = 0f;
     private float CurrentY = 0f;
@@ -25,6 +28,16 @@ public class CameraFollow : MonoBehaviour
 
     private void Update()
     {
+        GetMouthPos();
+    }
+
+    private void LateUpdate()
+    {
+        CameraMove();
+    }
+
+    private void GetMouthPos()
+    {
         CurrentX += Input.GetAxis("Mouse X") * Sensitivity;         //マウスX軸取得
         CurrentY -= Input.GetAxis("Mouse Y") * Sensitivity;         //マウスY軸取得
 
@@ -34,15 +47,35 @@ public class CameraFollow : MonoBehaviour
         CurrentY = Mathf.Clamp(CurrentY, AngleMIN, AngleMAX);
     }
 
-    private void LateUpdate()
+    private void CameraMove()
     {
+        //-Distance：プレイヤーの後ろに配置
         Vector3 Direction = new Vector3(0, 0, -Distance);
 
         //CurrentYとCurrentXを使って回転を計算
+        //Quaternion.Euler：ｚ、ｘ、ｙの順で回転する
         Quaternion Rotation = Quaternion.Euler(CurrentY, CurrentX, 0);
 
-        //カメラ位置をプレイヤー位置と回転、オフセットを基に設定
-        transform.position = Player.position + Rotation * Direction + Offset;
+        //カメラ位置をプレイヤー位置と回転、オフセットを基に設定（通常の位置）
+        Vector3 DesiredPosition = Player.position + Rotation * Direction + Offset;
+
+        //Physics.SphereCast：プレイヤーから理想的な位置（DesiredPosition）に向かって球を飛ばし、障害物を検知
+        //Player.position：SphereCast開始位置
+        //SphereCastRadius：SphereCastの半径
+        //DesiredPosition - Player.position：SphereCastの方向
+        RaycastHit hit;
+        if (Physics.SphereCast(Player.position, SphereCastRadius, DesiredPosition - Player.position, out hit, Distance, CollisionLayers))
+        {
+            //障害物が検出された場合、カメラを障害物の手前に配置
+            //hit.point：衝突地点
+            //hit.normal：衝突のベクトル
+            transform.position = hit.point + hit.normal * SphereCastRadius;
+        }
+        else
+        {
+            //障害物がない場合、通常の位置にカメラを配置
+            transform.position = DesiredPosition;
+        }
 
         //transform.LookAt：カメラに特定の位置を向かせ続ける
         //カメラがプレイヤーの位置を向くよう設定
