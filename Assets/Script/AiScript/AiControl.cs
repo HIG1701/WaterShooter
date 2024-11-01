d55b861c0f2b7ad8678c7ae5a5b31c55a372dea8
hama3238 <hamahama0410@i.softbank.jp>
2024-11-02 00:16:25 +0900

    Ai検証環境構築中
    Aiをどうやって作成するか検証中

---
+++ Assets/Script/AiScript/AiControl.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAIController : MonoBehaviour
+{
+    [SerializeField] private Transform player; 
+    [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private float sightRange = 20f;
+    [SerializeField] private float shootRange = 15f;
+    [SerializeField] private float healthFight = 30f;
+    private PlayerController playerController;
+    private NavMeshAgent agent;
+    private Vector3 lastKnownPosition;
+    private bool playerInSight = false;
+
+
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        CheckPlayerInSight();
+
+        if (playerInSight)
+        {
+            lastKnownPosition = player.position;
+            if (Vector3.Distance(transform.position, player.position) <= shootRange)
+            {
+
+            }
+            else
+            {
+                agent.SetDestination(player.position);
+            }
+        }
+        else if (lastKnownPosition != Vector3.zero)
+        {
+            agent.SetDestination(lastKnownPosition);
+        }
+    }
+
+    private void CheckPlayerInSight()
+    {
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= sightRange && !Physics.Raycast(transform.position, directionToPlayer, distanceToPlayer, obstacleLayer))
+        {
+            playerInSight = true;
+        }
+        else
+        {
+            playerInSight = false;
+        }
+    }
+}
