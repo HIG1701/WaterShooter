95cd664c3832cd64de51554b2eb8325e78b4b6f4
HIG1701 <166488883+HIG1701@users.noreply.github.com>
2024-11-21 23:46:34 +0900

    玉の処理をリジッドボディを用いて行う

--- Assets/Script/Gun/Bullet.cs
+++ Assets/Script/Gun/Bullet.cs
@@ -3,33 +3,29 @@ using UnityEngine;
 /// <summary>
 /// 弾丸に関するクラス
 /// </summary>
+[RequireComponent(typeof(Rigidbody))] //リジッドボディ必須
+//TODO;リジッドボディの影響か、プレイヤーが前方向に加速する
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private GunParameter gunParameter;
     private float speed;                                           //弾丸の速度
     private float range;                                           //射程距離
     private Vector3 startPosition;
+    private Rigidbody rb;
 
     private void Start()
     {
         speed = gunParameter.BulletSpeed;
         range = gunParameter.AttackRange;
         startPosition = transform.position;
+        rb = GetComponent<Rigidbody>();
+
+        //弾丸の初速度を設定
+        rb.velocity = transform.forward * speed;
     }
+
     private void FixedUpdate()
     {
-        AdvanceBullet();
-    }
-
-    /// <summary>
-    /// 弾丸を前方に進める
-    /// </summary>
-    private void AdvanceBullet()
-    {
-        //弾丸を前方に移動させる
-        //TODO:リジッドボディとTrigger
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
         //射程距離に達したら弾丸を破壊する
         if (Vector3.Distance(startPosition, transform.position) >= range) Destroy(gameObject);
     }
